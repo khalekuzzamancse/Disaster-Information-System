ca1ac409ce8e30986929f0312493c22d8510bd99
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2024-02-22 18:12:34 +0600

    implementing image gallery

--- app/src/main/java/com/khalekuzzaman/just/cse/datacollect/MainActivity.kt
+++ app/src/main/java/com/khalekuzzaman/just/cse/datacollect/MainActivity.kt
@@ -5,28 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import com.khalekuzzaman.just.cse.datacollect.ui.theme.DataCollectTheme
+import com.khalekuzzaman.just.cse.datacollect.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DataCollectTheme {
-                Column (
-                    modifier = Modifier.verticalScroll(rememberScrollState())
-                ){
-                    Text(text = "Welcome to Data collect")
-                    PermissionIfNeeded()
-                    MultiplePhotoPicker()
-                    SingleVideoPicker()
-                }
-
+            AppTheme{
+                MultiplePhotoPicker()
+//                Column (
+//                    modifier = Modifier.verticalScroll(rememberScrollState())
+//                ){
+//                    Text(text = "Welcome to Data collect")
+//                    PermissionIfNeeded()
+//
+//                    SingleVideoPicker()
+//                }
             }
         }
     }
--- app/src/main/java/com/khalekuzzaman/just/cse/datacollect/MultiplePhotoPicker.kt
+++ app/src/main/java/com/khalekuzzaman/just/cse/datacollect/MultiplePhotoPicker.kt
@@ -4,63 +4,228 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.khalekuzzaman.just.cse.datacollect.common_ui.GalleryScreen
+import com.khalekuzzaman.just.cse.datacollect.common_ui.command_pattern.Command
+import com.khalekuzzaman.just.cse.datacollect.common_ui.command_pattern.ImageGalleryCommands
+import com.khalekuzzaman.just.cse.datacollect.common_ui.command_pattern.UndoManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 
+@Immutable
+data class GalleryImage(
+    val uri: Uri,
+    val isSelected: Boolean = false
+)
 
-@Composable
-fun MultiplePhotoPicker(){
-    LocalContext.current
-    var imageUris by remember{
-        mutableStateOf<List<Uri>>(emptyList())
+@Immutable
+data class ImageGalleryState(
+    val images: List<GalleryImage> = emptyList()
+)
+
+class ViewModel {
+    private val _imageGalleryState = MutableStateFlow(ImageGalleryState(emptyList()))
+    val imageGalleryState = _imageGalleryState.asStateFlow()
+    val anySelected: Flow<Boolean> = _imageGalleryState.map { state ->
+        state.images.any { it.isSelected }
+    }
+    private var lastCommand: Command? = null
+
+    fun addImages(uris: List<Uri>) {
+        val command = ImageGalleryCommands.Add(
+            images = uris.map { GalleryImage(uri = it) },
+            currentState = _imageGalleryState.value
+        )
+        undoManager.execute(command)
+        command.execute()
+        lastCommand = command
+        updateState(command.state)
     }
 
-    val multiplePhotoPicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickMultipleVisualMedia(),
-        onResult = {
-            imageUris = it
-        }
-    )
-    Column{
-        Button(onClick = {
-            multiplePhotoPicker.launch(
-                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-            )
-        }) {
-            Text("Pick Multiple Images")
+    fun remove() {
+        val command = ImageGalleryCommands.Remove(_imageGalleryState.value)
+        command.execute()
+        lastCommand = command
+        updateState(command.state)
+    }
 
-        }
-        imageUris.forEach {uri ->
-            AsyncImage(
-                model = uri,
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(250.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            HorizontalDivider()
+    fun undo() {
+        lastCommand?.let { command ->
+            command.undo()
+            when (command) {
+                is ImageGalleryCommands.Add -> {
+                    updateState(command.state)
+                }
+                is ImageGalleryCommands.Remove->{
+                    updateState(command.state)
+                }
+            }
 
         }
 
+    }
+
+    fun redo() {
+        lastCommand?.let { command ->
+            command.redo()
+            when (command) {
+                is ImageGalleryCommands.Add -> {
+                    updateState(command.state)
+                }
+                is ImageGalleryCommands.Remove->{
+                    updateState(command.state)
+                }
+            }
+
+        }
+
+    }
+
+    private fun updateState(imageGalleryState: ImageGalleryState) {
+        _imageGalleryState.update { imageGalleryState }
+    }
+
+    fun flipSelection(currentSelected: Uri) {
+        val command = ImageGalleryCommands.FlipSelection(
+            currentState = _imageGalleryState.value,
+            uri = currentSelected
+        )
+        command.execute()
+        updateState(command.state)
     }
 
 
 }
+
+val viewModel = ViewModel()
+val undoManager = UndoManager()
+
+@Composable
+fun MultiplePhotoPicker() {
+    LocalContext.current
+    val showImageGallery = viewModel.imageGalleryState.collectAsState().value.images.isNotEmpty()
+    val images = viewModel.imageGalleryState.collectAsState().value.images
+    val showRemoveButton = viewModel.anySelected.collectAsState(false).value
+    val multiplePhotoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(),
+        onResult = viewModel::addImages
+    )
+    GalleryScreen(
+        enabledUndo = true,
+        enabledRedo = true,
+        showRemoveButton = showRemoveButton,
+        onExitRequest = { /*TODO*/ },
+        onAddRequest = {
+            multiplePhotoPicker.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        },
+        onRemoveRequest = viewModel::remove,
+        undoRequest = viewModel::undo,
+        redoRequest = viewModel::redo
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+            if (showImageGallery) {
+                ImageGallery(
+                    uiState = ImageGalleryState(images),
+                    onSelection = viewModel::flipSelection
+                )
+            }
+        }
+
+    }
+
+}
+
+
+@Composable
+fun ImageGallery(
+    uiState: ImageGalleryState,
+    onSelection: (Uri) -> Unit,
+) {
+
+    val state = rememberLazyStaggeredGridState()
+    LazyVerticalStaggeredGrid(
+        modifier = Modifier,
+        columns = StaggeredGridCells.Adaptive(120.dp),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalItemSpacing = 8.dp,
+        state = state,
+    ) {
+        items(
+            items = uiState.images,
+            itemContent = { image ->
+                Box(
+                    modifier = Modifier
+                        .clickable {
+                            viewModel.flipSelection(image.uri)
+                        }
+                        .clip(RoundedCornerShape(8.dp))
+                        .border(
+                            width = 2.dp,
+                            color = if (image.isSelected) Color.Red
+                            else MaterialTheme.colorScheme.tertiary,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                ) {
+                    Image(uri = image.uri)
+                    if (image.isSelected) {
+                        Checkbox(
+                            checked = true,
+                            onCheckedChange = {
+                                onSelection(image.uri)
+                            },
+                            modifier = Modifier.align(Alignment.BottomEnd)
+                        )
+                    }
+
+                }
+
+            }
+        )
+    }
+
+}
+
+@Composable
+private fun Image(
+    uri: Uri
+) {
+    AsyncImage(
+        model = uri,
+        contentDescription = null,
+        modifier = Modifier
+
+    )
+
+}
+
